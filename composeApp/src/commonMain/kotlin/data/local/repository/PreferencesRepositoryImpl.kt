ca33c7faac7652c2d50c37415248c973d1b1262b
otamurod <safarovotamurod@gmail.com>
2024-05-22 18:27:46 +0500

    feat: save & read source & target currencies

--- composeApp/src/commonMain/kotlin/data/local/repository/PreferencesRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/data/local/repository/PreferencesRepositoryImpl.kt
@@ -5,7 +5,10 @@ import com.russhwolf.settings.ObservableSettings
 import com.russhwolf.settings.Settings
 import com.russhwolf.settings.coroutines.FlowSettings
 import com.russhwolf.settings.coroutines.toFlowSettings
+import domain.model.CurrencyCode
 import domain.repository.PreferencesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
@@ -16,6 +19,11 @@ class PreferencesRepositoryImpl(
 ) : PreferencesRepository {
     companion object {
         const val TIME_STAMP_KEY = "lastUpdatedTime"
+        const val SOURCE_CURRENCY_KEY = "sourceCurrency"
+        const val TARGET_CURRENCY_KEY = "targetCurrency"
+
+        val DEFAULT_SOURCE_CURRENCY = CurrencyCode.USD.name
+        val DEFAULT_TARGET_CURRENCY = CurrencyCode.EUR.name
     }
 
     private val flowSettings: FlowSettings = (settings as ObservableSettings).toFlowSettings()
@@ -44,5 +52,33 @@ class PreferencesRepositoryImpl(
         } else {
             false
         }
+    }
+
+    override suspend fun saveSourceCurrencyCode(code: String) {
+        flowSettings.putString(
+            key = SOURCE_CURRENCY_KEY,
+            value = code
+        )
+    }
+
+    override suspend fun saveTargetCurrencyCode(code: String) {
+        flowSettings.putString(
+            key = TARGET_CURRENCY_KEY,
+            value = code
+        )
+    }
+
+    override fun readSourceCurrencyCode(): Flow<CurrencyCode> {
+        return flowSettings.getStringFlow(
+            key = SOURCE_CURRENCY_KEY,
+            defaultValue = DEFAULT_SOURCE_CURRENCY
+        ).map { CurrencyCode.valueOf(it) }
+    }
+
+    override fun readTargetCurrencyCode(): Flow<CurrencyCode> {
+        return flowSettings.getStringFlow(
+            key = TARGET_CURRENCY_KEY,
+            defaultValue = DEFAULT_TARGET_CURRENCY
+        ).map { CurrencyCode.valueOf(it) }
     }
 }
--- composeApp/src/commonMain/kotlin/domain/repository/PreferencesRepository.kt
+++ composeApp/src/commonMain/kotlin/domain/repository/PreferencesRepository.kt
@@ -1,6 +1,13 @@
 package domain.repository
 
+import domain.model.CurrencyCode
+import kotlinx.coroutines.flow.Flow
+
 interface PreferencesRepository {
     suspend fun saveLastUpdatedTime(lastUpdatedTime: String)
     suspend fun isDataFresh(currentTimeStamp: Long): Boolean
+    suspend fun saveSourceCurrencyCode(code: String)
+    suspend fun saveTargetCurrencyCode(code: String)
+    fun readSourceCurrencyCode(): Flow<CurrencyCode>
+    fun readTargetCurrencyCode(): Flow<CurrencyCode>
 }
