7fcce6ec1bf0faac109aaee0c71e75202255845a
otamurod <safarovotamurod@gmail.com>
2024-05-22 18:53:36 +0500

    feat: init Koin in iOSApp

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,16 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+
+    init() {
+        KoinModuleKt.initializeKoin()
+    }
+
 	var body: some Scene {
 		WindowGroup {
-			ContentView()
+			ContentView().ignoresSafeArea()
 		}
 	}
 }
